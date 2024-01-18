7a52249c31b6ef9c9d8ac7721c697975f24391bf
Chaya Segal <chayale90@gmail.com>
2024-01-18 21:29:22 +0200

    delete dist

--- dist/routes/configRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var indexR = require("./index");
-var usersR = require("./users");
-exports.routesInit = function (app) {
-    app.use("/", indexR);
-    app.use("/users", usersR);
-};
-//# sourceMappingURL=configRoutes.js.map
