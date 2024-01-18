281172de1bacbbbaaed9533eeae6994ebf187d77
Chaya Segal <chayale90@gmail.com>
2024-01-18 15:51:29 +0200

    order user-info route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { User } from "../models/userModel";
+import { User,UserProps } from "../models/userModel";
 const fs = require("fs");
 const express = require("express");
-const router = express.Router();
-const users = require("../customers.json");
 const path = require("path");
+const router = express.Router();
+const usersJson= require("../customers.json");
 
 // Check if the router "users" works
 router.get("/", (req: Request, res: Response) => {
@@ -14,7 +14,7 @@ router.get("/", (req: Request, res: Response) => {
 // get all users
 router.get("/users-list", async (req: Request, res: Response) => {
   try {
-    res.status(200).json(users);
+    res.status(200).json(usersJson);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ msg: "err", err });
@@ -24,19 +24,26 @@ router.get("/users-list", async (req: Request, res: Response) => {
 router.post("/add-user", async (req: Request, res: Response) => {
   try {
     const userData = req.body;
-    console.log(req.body);
-
     const user = new User(userData);
-    const data = fs.readFileSync(path.join(__dirname, "../customers.json"), "utf8");
-    const myUsers = JSON.parse(data);
-    myUsers.push(user);
+    const data = fs.readFileSync(
+      path.join(__dirname, "../customers.json"),
+      "utf8"
+    );
+    const currentUsers = JSON.parse(data);
+    currentUsers.push(user);
+    const jsonData = JSON.stringify(currentUsers);
 
-    const jsonData = JSON.stringify(myUsers);
-
-    fs.writeFile(path.join(__dirname, "../customers.json"), jsonData, (error: Error) => {
-      if (error) console.error("Error writing to file:", error);
-      return res.status(201).json({ message: "User added successfully!" });
-    });
+    fs.writeFile(
+      path.resolve(__dirname, "../customers.json"),
+      jsonData,
+      (error: Error) => {
+        if (error) {
+          console.error("Error writing to file:", error);
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+        return res.status(201).json({ message: "User added successfully!" });
+      }
+    );
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal Server Error" });
@@ -45,10 +52,9 @@ router.post("/add-user", async (req: Request, res: Response) => {
 
 router.get("/user-info/:userEmail", async (req: Request, res: Response) => {
   try {
-    const userEmail = req.params.userEmail;
-    const userInfo = users.find((user:User) => user.getEmail() === userEmail);
-    if (!userInfo) 
-    return res.status(404).json({ message: "User not found" });
+    const { userEmail } = req.params;
+    let userInfo = usersJson.find((user: UserProps) => user.email === userEmail);    
+    if (!userInfo) return res.status(404).json({ message: "User not found" });
     return res.status(200).json(userInfo);
   } catch (err) {
     console.log(err);
