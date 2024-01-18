726c53837586ea667c03f18f02d474e7c834c308
Chaya Segal <chayale90@gmail.com>
2024-01-18 21:24:10 +0200

    chnange tsConfig.json

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-    res.json({ msg: "api work" });
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
