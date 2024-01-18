9f700c30ca8ab5daa8ffe30d634bc01a9471701a
Chaya Segal <chayale90@gmail.com>
2024-01-18 17:59:36 +0200

    hi and json

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { User,UserProps } from "../models/userModel";
+import { User, UserProps } from "../models/userModel";
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-const usersJson= require("../customers.json");
+const usersJson = require("../customers.json");
 
 // Check if the router "users" works
 router.get("/", (req: Request, res: Response) => {
@@ -14,7 +14,14 @@ router.get("/", (req: Request, res: Response) => {
 // get all users
 router.get("/users-list", async (req: Request, res: Response) => {
   try {
-    res.status(200).json(usersJson);
+    let dataJson = fs.readFileSync(
+      path.join(__dirname, "../customers.json"),
+      "utf8"
+    );
+    console.log("hi");
+    
+    const currentUsers = JSON.parse(dataJson);
+    return res.status(200).json(currentUsers);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ msg: "err", err });
@@ -53,7 +60,9 @@ router.post("/add-user", async (req: Request, res: Response) => {
 router.get("/user-info/:userEmail", async (req: Request, res: Response) => {
   try {
     const { userEmail } = req.params;
-    let userInfo = usersJson.find((user: UserProps) => user.email === userEmail);    
+    let userInfo = usersJson.find(
+      (user: UserProps) => user.email === userEmail
+    );
     if (!userInfo) return res.status(404).json({ message: "User not found" });
     return res.status(200).json(userInfo);
   } catch (err) {
